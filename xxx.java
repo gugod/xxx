81bc62f046659d3758feaf5c8124589196a1e26a
Kang-min Liu <gugod@gugod.org>
2018-08-24 15:56:20 +0900

    java

---
+++ xxx.java
@@ -0,0 +1,10 @@
+// javac xxx.java && java xxx
+
+class xxx {
+    public static void main(String[] args) {
+        int x = 41,ﾠx = 42, xﾠ= 43;
+        System.out.println( x );
+        System.out.println(ﾠx );
+        System.out.println( xﾠ);
+    }
+}
