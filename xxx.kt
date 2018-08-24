52c2c09f58f78e5de96fee05d52a233a85c6658e
Kang-min Liu <gugod@gugod.org>
2018-08-24 16:58:24 +0900

    kotlin

---
+++ xxx.kt
@@ -0,0 +1,11 @@
+// kotlinc ./xxx.kt -include-runtime -d xxx.jar; java -jar ./xxx.jar
+
+fun main(args : Array<String>) {
+    val  x = 41
+    val ﾠx = 42
+    val  xﾠ= 43
+
+    println( x )
+    println(ﾠx )
+    println( xﾠ)
+}
